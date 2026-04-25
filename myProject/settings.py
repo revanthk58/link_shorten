c897f6f50e033f49fbff45e49f041e65812315b3
Kanna  Revanth Kumar <149651478+revanthk58@users.noreply.github.com>
2026-04-25 06:57:25 +0530

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!0nnfs&*-$rcb-zzqt08pt_tc%#@5ek*vd^^188^x9q9w4-f^r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
